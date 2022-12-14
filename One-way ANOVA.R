e93102248530a6cbc336b8fd4bb97f7ccc298dfe
KentPhamUky <kent.pham@uky.edu>
2022-12-14 11:26:04 -0500

    Update One-way ANOVA.R
    14Dec2022 Version 1.0 of one-way anova

--- One-way ANOVA.R
+++ One-way ANOVA.R
@@ -8,7 +8,6 @@
 exampleaov <- aov(Y ~ X, data = datasetname)
 summary(exampleaov)
 
-
 #Example one-way anova
 library(datasets)
 data(iris)
@@ -17,6 +16,8 @@ summary(speciesaov)
 
 
 
+#Note: If you have a numerical independent variable, convert it to a factor with following code
+dataset$columnname <- as.factor(dataset$columnname)
 
-#template one-way anova with dplyr
-library(dplyr)
+#example
+iris$Petal.Width <- as.factor(iris$Petal.Width)
