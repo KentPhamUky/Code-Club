0d403e66a3f94668e3b967b6ab0e5f78512a0412
Scott Culbert <120585477+scooteroo@users.noreply.github.com>
2022-12-16 14:56:53 -0500

    ColorBlind_Palette.R
    Contains an object with a color-blind-friendly palette that can be used for figures.
    
    Created 12dec2022

---
+++ ColorBlind_Palette.R
@@ -0,0 +1,14 @@
+#Script Name: Color Blind Palette 
+#Last Author: Scott C. 
+#Last Edit Date: 16Dec2022
+
+#The following object contains a color blind friendly palette that should be used for publishing figures
+
+cbp <- c("#000000", #Black
+         "#E69F00", #Orange
+         "#56B4E9", #Blue
+         "#009E73", #Green
+         "#F0E442", #Yellow
+         "#0072B2", #Dark Blue
+         "#D55E00", #Dark Orange
+         "#CC79A7") #Pink
