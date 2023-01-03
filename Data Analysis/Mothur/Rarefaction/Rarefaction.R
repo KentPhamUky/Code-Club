f5cb7900e4e8959279c8fc4bddf1965bbd2957ad
KentPhamUky <kent.pham@uky.edu>
2023-01-03 12:23:20 -0500

    Update to Rarefaction
    3Jan2023
    Added line to convert data to tall format which allows for plotting.
    Issue is that it's horribly inefficient and bricks weaker computers. Need to find better way to plot rarefaction or reduce the amount being plotted.

--- Data Analysis/Mothur/Rarefaction/Rarefaction.R
+++ Data Analysis/Mothur/Rarefaction/Rarefaction.R
@@ -6,11 +6,19 @@
 
 library(tidyverse) #For reading in summary file and ggplot2
 library(dplyr) #For Processing dataset
+library(reshape2) #For converting rarefaction dataset into tall format
 
-setwd("D:/GitHub/Code-Club/Data Analysis/Mothur/Rarefaction") #Change to your working directory where files are stored
+setwd("C:/Users/kdph224/Documents/GitHub/Code-Club/Data Analysis/Mothur/Rarefaction") #Change to your working directory where files are stored
 rawdataset <- read_tsv(file="Example.rarefaction", col_types = cols(numsampled=col_character())) #Read in your summary file containing alpha diversity
 
-df_sub <- select(rawdataset, -contains("lci-"))
-df <- select(df_sub, -contains("hci-"))
+df_sub <- select(rawdataset, -contains("lci-")) #Removes lci- columns
+df <- select(df_sub, -contains("hci-")) #Removes hci- columns
 
-plot <- ggplot(data = df, aes(x=numsampled, y=))
+
+df <- melt(df ,  id.vars = 'numsampled', variable.name = 'Sample') #Converts rarefaction table into tall format for graphing
+
+
+
+plot <- ggplot(data = df, aes(x=numsampled, y=value, color = Sample)) + #Plots rarefaction
+  geom_point()
+plot
